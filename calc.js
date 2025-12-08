40f960fc38ef28b2bc148bc26a2c491dad55f9c7
Grant Roode <109985613+Cat5-0@users.noreply.github.com>
2025-12-08 19:41:35 +0200

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,41 @@
+let numb1 = 0;
+let numb2 = 0;
+let operator = "";
+
+function operate(operator, numb1, numb2) {
+    switch (operator) {
+        case '+':
+            return add(numb1, numb2);
+        case '-':
+            return subtract(numb1, numb2);
+        case '*':
+            return multiply(numb1, numb2);
+        case '/':
+            return divide(numb1, numb2);
+        default:
+            return "Invalid operator";
+    }
+}
+
+function add(a, b) {
+    return a + b;
+}
+console.log("Add", add(1, 2));
+
+function subtract(c, d) {
+    return c - d;
+}
+console.log("Subtract", subtract(10, 56));
+
+function multiply(e, f) {
+    return e * f;
+}
+console.log("Multiply", multiply(5, 7));
+
+function divide(g, h) {
+    if (h === 0) {
+        return "Cannot divide by zero";
+    }
+    return g / h;
+}
+console.log("Divide", (25, 5));
