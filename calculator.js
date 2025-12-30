90bc219d7094851c817a38da0513e7daf1896873
Grant Roode <109985613+Cat5-0@users.noreply.github.com>
2025-12-30 15:07:25 +0200

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,30 @@
+// 1. press first number - save first number
+// 2. press operator - save operator
+// 3. press second number - save second number
+// 4. press equal sign - make calculation
+
+let firstNumber;
+let secondNumber;
+
+let operation;
+
+
+function clearDisplay() {
+    console.log("clear");
+}
+
+function getNumber(num) {
+    console.log(num);
+}
+
+function getOperator(operator) {
+    console.log(operator);
+}
+
+function getSymbol(symbol) {
+    console.log(symbol);
+}
+
+function getEquals(equals) {
+    console.log(equals);
+}
